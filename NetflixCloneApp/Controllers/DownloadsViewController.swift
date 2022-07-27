9e4dce24ec1a3d665d4b7ad4e047a0f217c63774
Nikolai Maximov <kolkamaximov@gmail.com>
2022-07-27 22:06:39 +0300

    app and start screen customization

---
+++ NetflixCloneApp/Controllers/DownloadsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DownloadsViewController.swift
+//  NetflixCloneApp
+//
+//  Created by Nikolai Maksimov on 27.07.2022.
+//
+
+import UIKit
+
+class DownloadsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+
+    }
+
+}
---
+++ NetflixCloneApp/Controllers/SearchViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SearchViewController.swift
+//  NetflixCloneApp
+//
+//  Created by Nikolai Maksimov on 27.07.2022.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+    
+
+ 
+
+}
