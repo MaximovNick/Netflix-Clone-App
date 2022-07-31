3ec12f373f178fe794353c06a75778a01de482ff
Nikolai Maximov <kolkamaximov@gmail.com>
2022-07-31 20:19:49 +0300

    Creating Upcoming TableView inside Upcoming Tab

--- NetflixCloneApp/Controllers/UpcomingViewController.swift
+++ NetflixCloneApp/Controllers/UpcomingViewController.swift
@@ -8,14 +8,61 @@
 import UIKit
 
 class UpcomingViewController: UIViewController {
+    
+    private var titles: [Title] = []
+    
+    private let upcomingTable: UITableView = {
+       let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
+        title = "Upcoming"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        
+        view.addSubview(upcomingTable)
+        upcomingTable.delegate = self
+        upcomingTable.dataSource = self
+        
+        fetchUpcoming()
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        upcomingTable.frame = view.bounds
+    }
+    
+    private func fetchUpcoming() {
+        NetworkManager.shared.getUpcomingMovies { [weak self] result in
+            switch result {
+            case .success(let titles):
+                self?.titles = titles
+                DispatchQueue.main.async {
+                    self?.upcomingTable.reloadData()
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
 
-   
-
+extension UpcomingViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        titles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text = titles[indexPath.row].original_name ?? titles[indexPath.row].original_title ?? "Unknowne"
+        cell.contentConfiguration = content
+        
+        return cell
+    }
 }
