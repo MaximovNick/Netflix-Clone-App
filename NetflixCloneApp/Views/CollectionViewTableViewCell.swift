324f5e10e310f35f156e9cbf1101c395287e2758
Nikolai Maximov <kolkamaximov@gmail.com>
2022-09-05 21:44:01 +0300

    add DataPersistenceManager

--- NetflixCloneApp/Views/CollectionViewTableViewCell.swift
+++ NetflixCloneApp/Views/CollectionViewTableViewCell.swift
@@ -55,8 +55,16 @@ class CollectionViewTableViewCell: UITableViewCell {
     
     private func downloadTitleAt(indexPath: IndexPath) {
         
+        DataPersistenceManager.shared.downloadTitleWith(model: titles[indexPath.row]) { result in
+            switch result {
+            case .success():
+                print("downloaded to Database")
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
         
-        print("Downloading \(titles[indexPath.row].original_title)")
+    
     }
 }
 
